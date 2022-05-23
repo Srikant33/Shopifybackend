6de9d045405fd9eb74b42ef3656f3993e56de37f
Srikant33 <96463545+Srikant33@users.noreply.github.com>
2022-05-22 21:54:24 -0400

    delete

--- App.js
+++ App.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose');
 const { MongoDB } = require('./config');
 const ItemModel = require('./src/obj')
 
+const cors = require("cors");
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended:true }));
+app.use(cors());
 app.set ('view engine','ejs');
 
 mongoose.connect(MongoDB)
@@ -12,25 +17,62 @@ app.listen(3000)
 )
 .catch(()=>console.log("error"));
 
-app.get('/',(req,res) => {
+app.get('/',(req,res) => { 
     res.redirect('/all-items');
 });
 
-app.get('/add-item',(req,res) => { 
-    const item = new ItemModel({
-        title:'strawberry',
-        description:'a juicy fruit',
-        quantity:5,
-        warehouse_location:'New York - 1'
-    });
+app.post('/add-item',(req,res) => { 
+
+    const item = new ItemModel(req.body);
     item.save()
-    .then((result) => res.send(result))
-    .catch((err) => {console.log(err)});
+    then((result) => res.redirect('/all-items'))    .catch((err) => {console.log(err)});
 });
 
+app.get('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    ItemModel.findById(id)
+      .then(result => {
+        console.log(result);
+        res.render('edit', { item: result, page: 'Edit an Item' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+  app.post('/edit-item/:id', (req, res) => {
+    const id = req.params.id;
+    console.log("req")
+    console.log(req);
+    ItemModel.findByIdAndUpdate(id,req.body)
+      .then(result => {
+        res.redirect('/all-items');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
 app.get('/all-items',(req,res) => {
-    ItemModel.find()
+    ItemModel.find().sort({createdAt: -1})
     .then((result) =>  res.render('index', {items:result ,page:'Shopify Inventory'} ))
     .catch((err) => {console.log(err)});
  });
 
+ app.get('/add', (req, res) => {
+    res.render('add', { page: 'Add a new item' });
+  });
+
+// delete the inventory item 
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+
+    ItemModel.findByIdAndDelete(id)
+      .then(result => {
+        res.json({ redirect: '/' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
