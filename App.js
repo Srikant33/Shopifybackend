0ca98fa5d4e5979d855c552205d56214eb97566d
Srikant33 <96463545+Srikant33@users.noreply.github.com>
2022-05-22 21:47:45 -0400

    adding items

--- App.js
+++ App.js
@@ -2,16 +2,25 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const { MongoDB } = require('./config');
+const ItemModel = require('./src/obj')
 
 mongoose.connect(MongoDB)
 .then(()=> 
-console.log("success")
+app.listen(3000)
 )
 .catch(()=>console.log("error"));
 
 
-app.get('/about',(req,res) => { 
-res.send();
+app.get('/add-item',(req,res) => { 
+    const item = new ItemModel({
+        title:'strawberry',
+        description:'a juicy fruit',
+        quantity:5,
+        warehouse_location:'New York - 1'
+    });
+    item.save()
+    .then((result) => res.send(result))
+    .catch((err) => {console.log(err)});
 });
 
 app.listen(3000);
